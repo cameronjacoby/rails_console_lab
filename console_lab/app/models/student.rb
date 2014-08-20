eb54f94e4dd538a900458221858d4de6b95f2d93
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-19 22:18:07 -0700

    add more validation

--- console_lab/app/models/student.rb
+++ console_lab/app/models/student.rb
@@ -1,11 +1,29 @@
 class Student < ActiveRecord::Base
-  
+
   validates :first_name,
     :presence => true,
-    :length => {:minimum => 5}
+    :length => {:minimum => 5},
+    :format => {:with => /[A-Z][a-z]+/}
 
   validates :last_name,
     :presence => true,
     :length => {:minimum => 5},
+    :format => {:with => /[A-Z][a-z]+/},
     :uniqueness => true
+
+  FORBIDDEN_NAMES = [{:first_name => "Delmer", :last_name => "Reed"},
+    {:first_name => "Tim", :last_name => "Licata"},
+    {:first_name => "Anil", :last_name => "Bridgpal"},
+    {:first_name => "Elie", :last_name => "Schoppik"}]
+
+  validate :name_is_allowed
+
+  def name_is_allowed
+    FORBIDDEN_NAMES.each do |name|
+      if name.first_name == first_name && name.last_name == last_name
+        errors.add(:first_name, :last_name, "this is a restricted name")
+      end
+    end
+  end
+
 end
