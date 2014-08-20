8ef23836ca7b9581e8083dd6ddc0b2a7105117b5
cameronjacoby <cameron.jacoby@gmail.com>
2014-08-19 17:02:17 -0700

    add first_name & last_name validations

--- console_lab/app/models/student.rb
+++ console_lab/app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ActiveRecord::Base
+  
+  validates :first_name,
+    :presence => true,
+    :length => {:minimum => 5}
+
+  validates :last_name,
+    :presence => true,
+    :length => {:minimum => 5},
+    :uniqueness => true
 end
